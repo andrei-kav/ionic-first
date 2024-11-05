025538af53ccdd45395807a797b9976b148de550
Andrei Kryvulia <andrei.k@datylon.com>
2024-11-05 15:02:27 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'menuIo',
+  webDir: 'www'
+};
+
+export default config;
